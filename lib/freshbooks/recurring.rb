33bc13c27b0e057a28e65cee5275e32463f66e92
Andy Fleener <anfleene@gmail.com>
2012-01-30 10:49:55 -0600

    Change PO Number to a string

--- lib/freshbooks/recurring.rb
+++ lib/freshbooks/recurring.rb
@@ -2,9 +2,9 @@ module FreshBooks
   class Recurring < FreshBooks::Base
     define_schema do |s|
       s.string :first_name, :last_name, :organization, :p_street1, :p_street2, :p_city
-      s.string :p_state, :p_country, :p_code, :lines, :notes, :terms, :frequency
+      s.string :p_state, :p_country, :p_code, :lines, :notes, :terms, :frequency, :po_number
       s.date :date
-      s.fixnum :recurring_id, :client_id, :po_number, :occurrences
+      s.fixnum :recurring_id, :client_id, :occurrences
       s.float :discount, :amount
       s.array :contact
       s.array :lines
